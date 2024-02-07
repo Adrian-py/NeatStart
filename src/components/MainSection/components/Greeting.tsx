eeb68df77e851d5e7532a08d613f9fc1fa8943a4
Adrian-py <62276355+Adrian-py@users.noreply.github.com>
2024-02-07 13:08:10 +0700

    Feat: add blinking animation to time colon

--- src/components/MainSection/components/Greeting.tsx
+++ src/components/MainSection/components/Greeting.tsx
@@ -1,24 +1,23 @@
 import { useEffect, useState } from 'react';
 
 type currentTimeType = {
-  time: string;
+  hour: string;
+  minute: string;
   date: string;
 };
 
 const Greeting = () => {
-  const [currentTime, setCurrentTime] = useState<currentTimeType>({ time: '00:00 AM', date: '01/01/2023' });
+  const [currentTime, setCurrentTime] = useState<currentTimeType>({ hour: "00", minute: "00", date: '01/01/2023' });
 
   // Update current time
   const updateTime = () => {
     const currentDate = new Date();
 
-    let time = currentDate.toLocaleString("id-ID", { hour: '2-digit', 'minute': '2-digit' }).replace('.', ':');
+    const time = currentDate.toLocaleString("id-ID", { hour: '2-digit', 'minute': '2-digit' });
+    const [hour, minute] = time.split(".");
     const date = currentDate.toLocaleDateString("id-ID", { day: 'numeric', month: 'numeric', year: 'numeric' }).replaceAll("/", " / ");
 
-    if (currentDate.getHours() >= 12) time += " PM";
-    else time += " AM";
-
-    setCurrentTime({ time: time, date: date });
+    setCurrentTime({ hour: hour, minute: minute, date: date });
   };
 
   useEffect(() => {
@@ -30,7 +29,7 @@ const Greeting = () => {
   return (
     <div className="greeting">
       <h1 className="greeting__hello">Hello!</h1>
-      <h2 className="greeting__time">{currentTime.time}</h2>
+      <h2 className="greeting__time">{currentTime.hour}<span className="greeting__time__colon">:</span>{currentTime.minute} {parseInt(currentTime.hour) >= 12 ? "PM" : "AM"}</h2>
       <h2 className="greeting__date">{currentTime.date}</h2>
     </div>
   );
